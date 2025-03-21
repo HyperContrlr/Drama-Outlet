bafa37a5c4c1dd164352f8d5b3c3af436f417d73
MUKILTEO\1717794 <hypercontrlrtwitch@gmail.com>
2025-03-21 13:19:37 -0700

    tilemap cont.

---
+++ Drama Outlet/Assets/Ethan/scripts/Building.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public bool Placed { get; private set; }
+    public BoundsInt area;
+
+    private void Start()
+    {
+        
+    }
+
+    #region Build Methods
+    #endregion
+
+}
---
+++ Drama Outlet/Assets/Ethan/scripts/GridBuildingSystem.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+//https://www.youtube.com/watch?v=gFpmJtO0NT4    6:42
+
+public class GridBuildingSystem : MonoBehaviour
+{
+    public static GridBuildingSystem current;
+
+    public GridLayout gridLayout;
+    public Tilemap MainTilemap;
+    public Tilemap TempTilemap;
+
+    public static Dictionary<TileType, TileBase> tileBases = new Dictionary<TileType, TileBase>();
+
+    #region Unity Methods
+    private void Awake()
+    {
+        current = this;
+    }
+
+    private void Start()
+    {
+        string tilePath = @"Tiles\";
+        tileBases.Add(TileType.Empty, null);
+        tileBases.Add(TileType.White, Resources.Load<TileBase>(tilePath + "white"));
+        tileBases.Add(TileType.Green, Resources.Load<TileBase>(tilePath + "green"));
+        tileBases.Add(TileType.Red, Resources.Load<TileBase>(tilePath + "red"));
+    }
+
+    private void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Tilemap Management
+
+    private static TileBase[] GetTileBlock (BoundsInt area, Tilemap tilemap)
+    {
+        TileBase[] array = new TileBase[area.size.x * area.size.y * area.size.z];
+        int counter = 0;
+
+        foreach (var v in area.allPositionsWithin)
+        {
+            Vector3Int pos = new Vector3Int(v.x, v.y, 0);
+            array[counter] = tilemap.GetTile(pos);
+            counter++;
+        }
+        return array;
+    }
+
+    private static void SetTilesBlock(BoundsInt area, TileType type, Tilemap tilemap)
+    {
+        int size = area.size.x * area.size.y * area.size.z;
+        TileBase[] tileArray = new TileBase[size];
+        FillTiles(tileArray, type);
+        tilemap.SetTilesBlock(area, tileArray);
+    }
+
+    private static void FillTiles(TileBase[] arr, TileType type)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = tileBases[type];
+        }
+    }
+    #endregion
+
+    #region Building Placement
+
+    #endregion
+}
+
+public enum TileType
+{
+    Empty,
+    White,
+    Green,
+    Red
+}
