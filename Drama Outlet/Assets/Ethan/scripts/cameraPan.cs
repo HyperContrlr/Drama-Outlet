7b303dda78fda990e8d246535d5d90771171b07c
MUKILTEO\1717794 <hypercontrlrtwitch@gmail.com>
2025-04-24 13:10:46 -0700

    camera pan no work rn :( HOWEVER; adjusted prefab hitboxes back to original & zoom is fully functional

--- Drama Outlet/Assets/Ethan/scripts/cameraPan.cs
+++ Drama Outlet/Assets/Ethan/scripts/cameraPan.cs
@@ -5,31 +5,24 @@ public class cameraPan : MonoBehaviour
 {
     public float zoomSpeed = 10f;
     public Camera cam;
-    public Vector3 offset;
+    private Vector3 lastMousePosition;
+    public float zOffset;
     private void Start()
     {
         cam = Camera.main;
     }
-    private Vector3 lastMousePosition;
-
     void Update()
     {
         float scroll = Mouse.current.scroll.ReadValue().y;
         cam.orthographicSize -= scroll * zoomSpeed * Time.deltaTime;
-        //if (Input.GetMouseButtonDown(2))
-        //{
-        //    lastMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-        //}
-
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, 1f, 10f);
         if (Input.GetMouseButton(2))
         {
             Vector3 currentMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
             Vector3 delta = lastMousePosition - currentMousePosition;
             cam.transform.position += delta;
-            cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, offset.z);
+            //cam.transform.position += new Vector3(delta.x, delta.y, zOffset);
             lastMousePosition = currentMousePosition;
         }
     }
-     
-    
 }
