1771b21313cda77315a14d88d849cf7175703b5d
MUKILTEO\1717794 <hypercontrlrtwitch@gmail.com>
2025-04-24 12:40:34 -0700

    HOPEFULLY CAMERA PAN WORKS

--- Drama Outlet/Assets/Ethan/scripts/GridSnap.cs
+++ Drama Outlet/Assets/Ethan/scripts/GridSnap.cs
@@ -1,1 +1,14 @@
-using UnityEngine;public class GridSnap : MonoBehaviour{private Grid grid;void Start(){grid = Grid.FindFirstObjectByType<Grid>(); } void Update() { Vector3Int yurr = grid.LocalToCell(transform.localPosition); transform.localPosition = grid.GetCellCenterLocal(yurr); }}
+using UnityEngine;
+public class GridSnap : MonoBehaviour
+{
+    private Grid grid;
+    void Start()
+    {
+        grid = Grid.FindFirstObjectByType<Grid>(); 
+    } 
+    void Update() 
+    { 
+        Vector3Int yurr = grid.LocalToCell(transform.localPosition); 
+        transform.localPosition = grid.GetCellCenterLocal(yurr); 
+    }
+}
--- Drama Outlet/Assets/Ethan/scripts/cameraPan.cs
+++ Drama Outlet/Assets/Ethan/scripts/cameraPan.cs
@@ -1,130 +1,35 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
 
-/*public class cameraPan : MonoBehaviour
+public class cameraPan : MonoBehaviour
 {
-    // Set the min and the max size for this 
-    // camera so that we can fix the limits
-    // of our zoom in and zoom out.
-    [SerializeField]
-    float CameraSizeMin = 1.0f;
-    [SerializeField]
-    float CameraSizeMax = 10.0f;
-    // The slider for zoom-in and zoom-out
-    [SerializeField]
-    public Slider SliderZoom;
-    // Some variables needed for dragging our 
-    // camera to creae the pan control
-    private Vector3 mDragPos;
-    private Vector3 mOriginalPosition;
-    private bool mDragging = false;
-    // The zoom factor
-    private float mZoomFactor = 0.0f;
-    // Save a reference to the Camera.main
-    private Camera mCamera;
-    public static bool IsCameraPanning
+    public float zoomSpeed = 10f;
+    public Camera cam;
+    public Vector3 offset;
+    private void Start()
     {
-        get;
-        set;
-    } = true;
-    void Start()
-    {
-        if (CameraSizeMax < CameraSizeMin)
-        {
-            float tmp = CameraSizeMax;
-            CameraSizeMax = CameraSizeMin;
-            CameraSizeMin = tmp;
-        }
-        if (CameraSizeMax - CameraSizeMin < 0.01f)
-        {
-            CameraSizeMax += 0.1f;
-        }
-        SetCamera(Camera.main);
+        cam = Camera.main;
     }
-    public void SetCamera(Camera camera)
-    {
-        mCamera = camera;
-        mOriginalPosition = mCamera.transform.position;
-        // For this demo, we simple take the current camera
-        // and calculate the zoom factor.
-        // Alternately, you may want to set the zoom factor
-        // in other ways. For example, randomize the 
-        // zoom factory between 0 and 1.
-        mZoomFactor =
-          (CameraSizeMax - mCamera.orthographicSize) /
-          (CameraSizeMax - CameraSizeMin);
-        if (SliderZoom)
-        {
-            SliderZoom.value = mZoomFactor;
-        }
-    }
-    public void Zoom(float value)
-    {
-        mZoomFactor = value;
-        // clamp the value between 0 and 1.
-        mZoomFactor = Mathf.Clamp01(mZoomFactor);
-        if (SliderZoom)
-        {
-            // if the slider is valied
-            // set the value to the slider.
-            SliderZoom.value = mZoomFactor;
-        }
-        // set the camera size
-        mCamera.orthographicSize = CameraSizeMax -
-            mZoomFactor *
-            (CameraSizeMax - CameraSizeMin);
-    }
-    public void ZoomIn()
-    {
-        // For this demo we hardcode 
-        // the increment. You should
-        // avoid hardcoding the value.
-        Zoom(mZoomFactor + 0.01f);
-    }
-    public void ZoomOut()
-    {
-        Zoom(mZoomFactor - 0.01f);
-    }
+    private Vector3 lastMousePosition;
 
     void Update()
     {
-        // Camera panning is disabled when a tile is selected.
-        if (!IsCameraPanning)
+        float scroll = Mouse.current.scroll.ReadValue().y;
+        cam.orthographicSize -= scroll * zoomSpeed * Time.deltaTime;
+        //if (Input.GetMouseButtonDown(2))
+        //{
+        //    lastMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        //}
+
+        if (Input.GetMouseButton(2))
         {
-            mDragging = false;
-            return;
-        }
-        // We also check if the pointer is not on UI item
-        // or is disabled.
-        if (EventSystem.current.IsPointerOverGameObject() ||
-          enabled == false)
-        {
-            //mDragging = false;
-            return;
-        }
-        // Save the position in worldspace.
-        if (Input.GetMouseButtonDown(0))
-        {
-            mDragPos = mCamera.ScreenToWorldPoint(
-              Input.mousePosition);
-            mDragging = true;
-        }
-        if (Input.GetMouseButton(0) && mDragging)
-        {
-            Vector3 diff = mDragPos -
-              mCamera.ScreenToWorldPoint(
-                Input.mousePosition);
-            diff.z = 0.0f;
-            mCamera.transform.position += diff;
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            mDragging = false;
+            Vector3 currentMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 delta = lastMousePosition - currentMousePosition;
+            cam.transform.position += delta;
+            cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, offset.z);
+            lastMousePosition = currentMousePosition;
         }
     }
-    public void OnSliderValueChanged()
-    {
-        Zoom(SliderZoom.value);
-    }
-}*/
+     
+    
+}
