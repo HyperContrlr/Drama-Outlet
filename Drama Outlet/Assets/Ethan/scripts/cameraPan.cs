1850a9fbe16dcf72220ad8e7f55285b47a1a5c60
MUKILTEO\1717794 <hypercontrlrtwitch@gmail.com>
2025-04-24 13:24:58 -0700

    did something ig

--- Drama Outlet/Assets/Ethan/scripts/cameraPan.cs
+++ Drama Outlet/Assets/Ethan/scripts/cameraPan.cs
@@ -3,22 +3,23 @@ using UnityEngine.InputSystem;
 
 public class cameraPan : MonoBehaviour
 {
-    public float zoomSpeed = 10f;
+    public float zoomSpeed = 50f;
     public Camera cam;
     private Vector3 lastMousePosition;
     public float zOffset;
     private void Start()
     {
         cam = Camera.main;
+        lastMousePosition = cam.transform.position;
     }
     void Update()
     {
         float scroll = Mouse.current.scroll.ReadValue().y;
         cam.orthographicSize -= scroll * zoomSpeed * Time.deltaTime;
         cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, 1f, 10f);
+        Vector3 currentMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButton(2))
         {
-            Vector3 currentMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
             Vector3 delta = lastMousePosition - currentMousePosition;
             cam.transform.position += delta;
             //cam.transform.position += new Vector3(delta.x, delta.y, zOffset);
