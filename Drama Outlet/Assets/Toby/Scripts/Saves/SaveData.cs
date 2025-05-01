d8f93a178525f85aa132bb715b3a43af7ec5edd4
MUKILTEO\1717222 <tobiahsday@gmail.com>
2025-05-01 13:24:20 -0700

    save data pt 1

---
+++ Drama Outlet/Assets/Toby/Scripts/Saves/SaveData.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct SaveData
+{
+    public enum Time { EarlyMorning, Morning, Afternoon, Evening, Night, Midnight }
+
+    public Time timeOfDay;
+
+    public float money;
+
+    public float day;
+
+    public bool isNewGame;
+
+    public float starsGained;
+
+    public float approvalValue;
+
+    public float securityValue;
+
+    public List<Vector2> furniturePositions;
+}
---
+++ Drama Outlet/Assets/Toby/Scripts/Saves/SaveDataController.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using Unity.Android.Gradle;
+using UnityEngine;
+
+public class SaveDataController : MonoBehaviour
+{
+    public static SaveDataController Instance;
+
+    public string fileName;
+
+    public SaveDataAsset defaultData;
+
+    private SaveData _currentData;
+    public ref SaveData CurrentData => ref _currentData;
+
+    [SerializeField] private List<products> allProducts;
+    [SerializeField] private List<buildings> allBuildings;
+    [SerializeField] private List<License> allLicenses;
+    public GameObject tutorial;
+    public void Awake()
+    {
+        Instance = this;
+        Load();
+    }
+
+    public void OnDestroy()
+    {
+        Save();
+    }
+    public void Save()
+    {
+        Serializer.Save(_currentData, $"{Application.persistentDataPath}/SaveData", fileName);
+    }
+
+    public void Load()
+    {
+        _currentData = Serializer.Load($"{Application.persistentDataPath}/SaveData", fileName, defaultData.value);
+        ResetObjects();
+    }
+
+    public void ResetObjects()
+    {
+        if (_currentData.isNewGame == true)
+        {
+            tutorial.SetActive(true);
+            foreach (var product in allProducts)
+            {
+                product.ResetValues();
+            }
+            foreach (var building in allBuildings)
+            {
+                building.ResetValues();
+            }
+            foreach (var license in allLicenses)
+            {
+                license.ResetValues();
+            }
+            _currentData.isNewGame = false;
+        }
+    }
+}
