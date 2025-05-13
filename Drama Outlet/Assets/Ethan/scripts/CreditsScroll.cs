130c1daaa8324018ade59c11b986b3ee3a62e9d8
MACH119\Tombutu <turtleloverday@gmail.com>
2025-05-13 16:04:45 -0700

    credits fix

--- Drama Outlet/Assets/Ethan/scripts/CreditsScroll.cs
+++ Drama Outlet/Assets/Ethan/scripts/CreditsScroll.cs
@@ -7,20 +7,34 @@ public class CreditsScroll : MonoBehaviour
     public RectTransform creditsPanel;
     public GameObject creditsPanelObject;
     public Vector2 initialPos;
+    public bool isScrolling;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //gets the component.. idk why I started doing comments for this script.
         creditsPanel = GetComponent<RectTransform>();
     }
+
+    public void Update()
+    {
+       if (isScrolling == true)
+       {
+           creditsPanel.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+       }
+       if (isScrolling == false)
+       {
+           creditsPanel.anchoredPosition = initialPos;
+       }
+    }
     public void StartDaScrolling()
     {
+        isScrolling = true;
         //does the entire thing that this script was created to do.
         creditsPanelObject.SetActive(true);
-        creditsPanel.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
     }
     public void StopDaScrolling()
     {
+        isScrolling = false;
         //stops the scrolling
         creditsPanel.anchoredPosition = initialPos;
         creditsPanelObject.SetActive(false);
